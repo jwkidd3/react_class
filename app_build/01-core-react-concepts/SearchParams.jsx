4bace14447a7c39bac9bc32736ef662a913b6c11
jwkidd3 <jkidd@kiddcorp.com>
2025-02-05 13:28:50 -0600

    Update SearchParams.jsx

--- app_build/01-core-react-concepts/SearchParams.jsx
+++ app_build/01-core-react-concepts/SearchParams.jsx
@@ -1,16 +1,37 @@
-import { useState } from "react";
 
-const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+import { useEffect, useState } from "react";
+import useBreedList from "./useBreedList";
+import Pet from "./Pet";
+import Results from "./Results";
+import './style.css'
 
 const SearchParams = () => {
+    const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
     const [location, setLocation] = useState("");
     const [animal, setAnimal] = useState("");
     const [breed, setBreed] = useState("");
-    const breeds = [];
+    const [pets, setPets] = useState([]);
+    const [breeds] = useBreedList(animal);
+
+    useEffect(() => {
+        requestPets();
+      }, []);
+
+      async function requestPets() {
+        const res = await fetch(
+          `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+        );
+        const json = await res.json();
+      
+        setPets(json.pets);
+      }
 
     return (
       <div className="search-params">
-        <form>
+        <form onSubmit={e => {
+            e.preventDefault();
+            requestPets();
+          }}>
           <label htmlFor="location">
             Location
            <input
@@ -62,6 +83,7 @@ const SearchParams = () => {
             </label>
           <button>Submit</button>
         </form>
+        <Results pets={pets} />;
       </div>
     );
   };
